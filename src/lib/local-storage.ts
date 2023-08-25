2a277a817455c123e6605b9209f226575a8be697
Hai Le <haile.vnm@gmail.com>
2023-08-25 09:16:40 +0700

    chore: add localstorage interaction functions

---
+++ src/lib/local-storage.ts
@@ -0,0 +1,16 @@
+type AppFields = 'access-token';
+
+const getAppKey = (key: AppFields) => `yasitan_${key}`;
+
+export const setField = (key: AppFields, value: string) => {
+  localStorage.setItem(getAppKey(key), value);
+};
+
+export const removeField = (key: AppFields) => {
+  localStorage.removeItem(getAppKey(key));
+};
+
+export const getField = (key: AppFields) =>
+  localStorage.getItem(getAppKey(key));
+
+export const getNumberField = (key: AppFields) => Number(getField(key));
