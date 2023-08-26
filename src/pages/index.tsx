dfa693bed613e4f429455e7923bdf4a6e8209c54
Hai Le <haile.vnm@gmail.com>
2023-08-27 00:17:25 +0700

    feat: say hi to current user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import { getLayout } from '@/components/layouts/home';
 import { embedPageLayout } from './_app';
+import { useProfileState } from '@/contexts/profile';
+import Link from 'next/link';
 
 export default function Page() {
+  const { user } = useProfileState();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <h1>Let&apos;s explore the Yasitan!</h1>
-        <button>Start a new conversation</button>
+        <h1>Welcome {user?.email}</h1>
+        <h3>
+          Let&apos;s explore the Yasitan!&nbsp;
+          <Link
+            className="font-medium text-cyan-200 hover:underline"
+            href="/conversations/new"
+          >
+            Start a new conversation
+          </Link>
+          &nbsp;🚀
+        </h3>
       </div>
     </main>
   );
