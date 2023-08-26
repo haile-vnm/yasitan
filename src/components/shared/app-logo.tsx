21b9ad95660d17816435d5483f1d9ef246226a33
Hai Le <haile.vnm@gmail.com>
2023-08-27 00:15:37 +0700

    feat: add app logo component

---
+++ src/components/shared/app-logo.tsx
@@ -0,0 +1,5 @@
+import { ChatBubbleLeftEllipsisIcon } from '@heroicons/react/24/outline';
+
+export default function AppLogo() {
+  return <ChatBubbleLeftEllipsisIcon className="text-cyan-200 w-20 h-20" />;
+}
